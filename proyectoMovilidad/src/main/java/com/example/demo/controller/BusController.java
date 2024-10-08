ad34b5f0e25a79bab811baa08f8152fa3e254052
Carlos Sebastian Gomez Vergel <carlozetox@gmail.com>
2024-10-08 13:22:42 -0500

    Bus Controller update

--- proyectoMovilidad/src/main/java/com/example/demo/controller/BusController.java
+++ proyectoMovilidad/src/main/java/com/example/demo/controller/BusController.java
@@ -1,11 +1,16 @@
 package com.example.demo.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.demo.entities.bus;
 import com.example.demo.services.BusService;
@@ -28,9 +33,49 @@ public class BusController{
 		return "admin/bus/index";
 	}
 	
-	@GetMapping("/addBus")
-	public String addBus() {
-		return "admin/bus/add";
+	@PostMapping("/addBus")
+	public String addBus(@ModelAttribute("bus") bus bus) { 
+	    try {
+	        busService.save(bus); 
+	    } catch (Exception e) {
+	        System.out.println("Error al agregar el bus: " + e);
+	    }
+	    return "redirect:/listBus";
 	}
 	
+	@GetMapping("/editBus/{id}")
+	public String editBus(@PathVariable("id") String id, Model model) {
+	    try {
+	        Optional<bus> bus = busService.findById(id);
+	        if (bus.isPresent()) {
+	            model.addAttribute("bus", bus.get());
+	            return "admin/bus/edit";
+	        } else {
+	            return "redirect:/listBus"; 
+	        }
+	    } catch (Exception e) {
+	        System.out.println("Error al obtener el bus: " + e);
+	        return "redirect:/listBus";
+	    }
+	}
+	
+	@PostMapping("/updateBus")
+	public String updateBus(@ModelAttribute("bus") bus bus) {
+	    try {
+	        busService.save(bus); 
+	    } catch (Exception e) {
+	        System.out.println("Error al actualizar el bus: " + e);
+	    }
+	    return "redirect:/listBus";
+	}
+	
+	@DeleteMapping("/deleteBus/{id}")
+	public String deleteBus(@PathVariable("id") String id) {
+	    try {
+	        busService.deleteById(id);
+	    } catch (Exception e) {
+	        System.out.println("Error al eliminar el bus: " + e);
+	    }
+	    return "redirect:/listBus";
+	}
 }
